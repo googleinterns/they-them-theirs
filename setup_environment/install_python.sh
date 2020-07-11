fe8b24f94b8dc6cf15fab1dacfe495568519c94f
Tony Sun <sunto@google.com>
2020-07-11 00:18:27 +0000

    adding scripts to set up environment with pyenv, python, and pytorch on GCP

---
+++ setup_environment/install_python.sh
@@ -0,0 +1,6 @@
+# using pyenv to install python
+# it will take some time (usually ~10 min) for the installation
+
+echo "Installing Python version 3.7.7..."
+pyenv install -v 3.7.7
+pyenv global 3.7.7
