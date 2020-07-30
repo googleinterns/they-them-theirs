2e825d7494423a9558e36bf9fb9e70f062525106
Tony Sun <sunto@google.com>
2020-07-30 12:12:46 -0700

    revamping data generation algorithm to do replacement by position

--- neutral_generation/constants.py
+++ neutral_generation/constants.py
@@ -1,10 +1,13 @@
 # pronouns
-NON_FUNCTION_PRONOUNS = {   # need to change pronoun based on context
+NON_FUNCTION_PRONOUNS = {  # need to change pronoun based on context
     # one-to-many mapping
-    'her': ['them',     # I talked to her --> I talked to them
-            'their'],   # This is her pen --> This is their pen
-    'his': ['their',    # This is his pen --> This is their pen
-            'theirs']   # This pen is his --> This pen is theirs
+    # using dependency parser for "her" --> "their" / "them", so not including "her" here
+    'his': ['their',  # This is his pen --> This is their pen
+            'theirs'],  # This pen is his --> This pen is theirs
+    "he's": ["they've",
+             "they're"],
+    "she's": ["they've",
+              "they're"]
 }
 
 NON_INJECTIVE_PRONOUNS = {
@@ -13,18 +16,18 @@ NON_INJECTIVE_PRONOUNS = {
     'she': 'they',
     "she's": "they're",
     "he's": "they're",
-    'herself': 'themself',  # TODO: be careful with cases such as "Sarah herself" --> "Sarah themself"
+    'herself': 'themself',
     'himself': 'themself'
 }
 
 INJECTIVE_PRONOUNS = {
     # one-to-one mapping
-    'him': 'them',      # I talked to him --> I talked to them
-    'hers': 'theirs'    # This pen is hers --> This pen is theirs
+    'him': 'them',  # I talked to him --> I talked to them
+    'hers': 'theirs'  # This pen is hers --> This pen is theirs
 }
 
 EASY_PRONOUNS = NON_INJECTIVE_PRONOUNS.copy()
-EASY_PRONOUNS.update(INJECTIVE_PRONOUNS)  # these pronouns can be replaced with regex
+EASY_PRONOUNS.update(INJECTIVE_PRONOUNS)  # these pronouns can be replaced directly
 
 OCCUPATION_WORDS = {
     'policeman': 'police officer',
@@ -35,7 +38,7 @@ OCCUPATION_WORDS = {
     'weatherman': 'weather reporter',
     'fireman': 'firefighter',
     'chairman': 'chair',
-    'spokesman': 'spokesperson'     # TODO: gradually add more words
+    'spokesman': 'spokesperson'  # TODO: gradually add more words
 }
 
 GENDER_SPECIFIC = {
@@ -44,23 +47,14 @@ GENDER_SPECIFIC = {
     'laymen': 'lay people',
 }
 
-GENDERED_TERMS = OCCUPATION_WORDS
+GENDERED_TERMS = OCCUPATION_WORDS.copy()
 GENDERED_TERMS.update(GENDER_SPECIFIC)
 
-
-# verbs
-RULE_CHANGE_VERBS = {
+IRREGULAR_VERBS = {
     'is': 'are',
-    'has': 'have',
-    'was': 'were'
-}
-
-IRREGULAR_PRESENT_SIMPLE_VERBS = {
-    'is': 'are',
-    "'s": "'re",
+    'was': 'were',
     'has': 'have',
     'does': 'do',
-    'doesn': 'don',
     'goes': 'go'
 }
 
