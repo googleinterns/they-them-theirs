35a4916c0caad852993dc8732af48add2086a5b3
Tony Sun <tonysun@ucsb.edu>
2020-08-08 16:39:52 -0700

    fixing highlight function for sentences with diff # of tokens (#7)
    * fixing highlight function for sentences with diff # of tokens
    
    Co-authored-by: Tony Sun <sunto@google.com>

--- flask_app/static/script.js
+++ flask_app/static/script.js
@@ -1,21 +1,45 @@
-function highlight(oldElem, newElem) {
-    var oldText = oldElem.text().split(' '),
-        newText = newElem.text().split(' '),
-        added = '',
-        removed = '';
+function highlightDifference(oldElem, newElem) {
+    // highlight the difference between the original sentence (oldElem) and the new sentence (newElem)
 
-    newText.forEach(function(val, i){
-        if (val != oldText[i]) {
-            added += "<span class='added'>" + val + "</span>";
-            removed += "<span class='removed'>" + oldText[i] + "</span>";
+    // list of words in the original and new sentences
+    let oldWords = get_words(oldElem),
+        newWords = get_words(newElem);
+
+    let removedWords = oldWords.filter(word => !newWords.includes(word)),  // list of removed words in original sentence
+        addedWords = newWords.filter(word => !oldWords.includes(word));  // list of added words in new sentence
+
+    var oldText = "",
+        newText = "";
+
+    // add the "removed" span tag to removed words in the original sentence
+    for (var i=0; i < oldWords.length; i++) {
+        if (removedWords.includes(oldWords[i])) {
+            oldText += "<span class='removed'>" + oldWords[i] + "</span>";
         } else {
-            added += val;
-            removed += oldText[i];
+            oldText += oldWords[i];
         }
-        added += " ";
-        removed += " ";
-    });
+        if (i < oldWords.length - 1) {
+            oldText += " "
+        }
+    }
 
-    oldElem.html(removed);
-    newElem.html(added);
+    // add the "added" span tag to added words in the new sentence
+    for (var i=0; i < newWords.length; i++) {
+        if (addedWords.includes(newWords[i])) {
+            newText += "<span class='added'>" + newWords[i] + "</span>";
+        } else {
+            newText += newWords[i];
+        }
+        if (i < newWords.length - 1) {
+            newText += " "
+        }
+    }
+
+    oldElem.html(oldText);
+    newElem.html(newText);
+}
+
+function get_words(element) {
+    // get the words of a sentence
+    return element.text().split(' ');
 }
